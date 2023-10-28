863c44249ae20ac5276c387dba790dc4683b02b8
guillermoscript <arepayquezo@gmail.com>
2023-10-28 16:07:18 -0400

    upload mutation

---
+++ components/chat/hooks/useMutationUploadAudio.ts
@@ -0,0 +1,31 @@
+import { apiUrl } from "@/env";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+type UseMutationUploadAudio = {
+	progress?: any;
+}
+
+export default function useMutationUploadAudio({
+	progress
+}: UseMutationUploadAudio) {
+	return useMutation({
+		mutationFn: async (file: File) => {
+			const formData = new FormData();
+			formData.append("file", file);
+			const res = await axios.post(`${apiUrl}/api/audios`, formData, {
+				withCredentials: true,
+				onUploadProgress(progressEvent) {
+					const percentCompleted = Math.round(
+						(progressEvent.loaded * 100) / progressEvent?.total!
+					);
+					if (progress) progress(percentCompleted);
+				},
+			});
+			return res.data;
+		},
+		onError(error, variables, context) {
+			console.log(error);
+		}
+	});
+}
---
+++ components/chat/hooks/useMutationUploadPdf.ts
@@ -0,0 +1,31 @@
+import { apiUrl } from "@/env";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+type useMutationUploadPdf = {
+	progress?: any;
+}
+
+export default function useMutationUploadPdf({
+	progress
+}: useMutationUploadPdf) {
+	return useMutation({
+		mutationFn: async (file: File) => {
+			const formData = new FormData();
+			formData.append("file", file);
+			const res = await axios.post(`${apiUrl}/api/user-documents`, formData, {
+				withCredentials: true,
+				onUploadProgress(progressEvent) {
+					const percentCompleted = Math.round(
+						(progressEvent.loaded * 100) / progressEvent?.total!
+					);
+					if (progress) progress(percentCompleted);
+				},
+			});
+			return res.data;
+		},
+		onError(error, variables, context) {
+			console.log(error);
+		}
+	});
+}
